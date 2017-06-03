9211d0d962a11e7a102d8ce73f1ea4f28f694627
Walter Chan <walterch87@gmail.com>
2017-06-03 21:36:53 +0800

    javascript support

---
+++ lanex.js
@@ -0,0 +1,25 @@
+$(document).ready(function(){
+
+	/*
+    $(this).click(function(){
+        
+            alert("The paragraph is now hidden");
+  
+    });*/
+
+/*
+	$("p").on("tap",function(){
+		alert("The paragraph is now hidden 1");
+	  //$(this).hide();
+
+	});
+
+	$("p").on("taphold",function(){
+		//$(this).hide();
+		$(this).text("Hello world!");
+		//alert("The paragraph is now hidden 2");
+	}); 
+*/
+
+
+});
